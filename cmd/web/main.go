bd8550127a2760d65066de18ea19aa772715312b
Diego Alba <d.alba.work@gmail.com>
2026-03-30 16:56:47 -0400

    feat: init the repo

---
+++ cmd/web/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/DAlba-sudo/pff"
+)
+
+// This is what the frontend will modify.
+func Frontend(a *pff.App) {
+
+}
+
+// This is all basic boiler plate, as the frontend you will not have to touch this.
+func main() {
+	templateDir := flag.String("template", "templates", "the directory for the templates")
+	staticDir := flag.String("static", "static", "the directory for the static files")
+	address := flag.String("address", "127.0.0.1", "the address to bind the server to")
+	port := flag.Int("port", 8080, "the port to bind the server to")
+	live := flag.Bool("reload", true, "whether to do live reloads on the template files")
+	flag.Parse()
+
+	app := pff.CreateApp(pff.Configuration{
+		TemplateDirectoryPath: *templateDir,
+		FileSystemPath:        *staticDir,
+		Address:               *address,
+		Port:                  *port,
+		Live:                  *live,
+	})
+
+	Frontend(app)
+
+	if err := app.Start(); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/CFF4HA/Dashboard
+
+go 1.26.1
+
+require (
+	github.com/DAlba-sudo/pbf v1.0.0 // indirect
+	github.com/DAlba-sudo/pff v1.0.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
