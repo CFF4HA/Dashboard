189e10d02eedfb9a71323884877d3663608fd53a
Diego Alba <d.alba.work@gmail.com>
2026-04-18 13:32:10 -0400

    feat: added go-native ingredient retrieval

---
+++ internal/handlers/ingredient/create.go
@@ -0,0 +1,156 @@
+package ingredient
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/CFF4HA/Dashboard/internal/core"
+	"github.com/CFF4HA/Dashboard/internal/types"
+	"github.com/CFF4HA/Dashboard/pkg/pubchem"
+	"github.com/google/uuid"
+	"github.com/tidwall/gjson"
+)
+
+// this file conatins the functions required to create a new ingredient in the database.
+
+// this will create an ingredient given a name, and will contact the PubChem API
+// directly. Do not assume that this is controlled via rate-limiting, etc. Call this
+// only when you directly want to create an ingredient, and not as part of a batch process.
+func RetrieveIngredient(name string) (*types.Ingredient, error) {
+	pubchem_id, err := pubchem.GetCompoundId(name)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := pubchem.GetCompoundAsJSON(pubchem_id)
+	if err != nil {
+		return nil, err
+	}
+
+	// the following section extracts from the data the relevant information to create
+	// an ingredient (i.e., the hazards, effects, regulations, etc.)
+	_signs_symptoms := gjson.GetBytes(data, `Record.Section.#(TOCHeading=="Toxicity").Section.#(TOCHeading=="Toxicological Information").Section.#(TOCHeading=="Signs and Symptoms").Information.#.Value.StringWithMarkup.#.String`)
+	_adverse_effects := gjson.GetBytes(data, `Record.Section.#(TOCHeading=="Toxicity").Section.#(TOCHeading=="Toxicological Information").Section.#(TOCHeading=="Adverse Effects").Information.#.Value.StringWithMarkup.#.String`)
+	_ghs_hazards := gjson.GetBytes(data, `Record.Section.#(TOCHeading=="Safety and Hazards").Section.#(TOCHeading=="Hazards Identification").Section.#(TOCHeading=="GHS Classification").Information.#(Name=="GHS Hazard Statements").Value.StringWithMarkup.#.String`)
+	_california_cosmetics := gjson.GetBytes(data, `Record.Section.#(TOCHeading=="Safety and Hazards").Section.#(TOCHeading=="Regulatory Information").Information.#(Name=="California Safe Cosmetics Program (CSCP) Reportable Ingredient").Value.StringWithMarkup.#.String`)
+	_names := gjson.GetBytes(data, `Record.Section.#(TOCHeading=="Names and Identifiers").Section.#(TOCHeading=="Synonyms").Section.#(TOCHeading=="Depositor-Supplied Synonyms").Information.#.Value.StringWithMarkup.#.String`)
+
+	var names []string
+	if err := json.Unmarshal([]byte(_names.Array()[0].String()), &names); err != nil {
+		core.Logger.Error("failed to unmarshal names for an ingredient", "name", name, "err", err)
+	}
+
+	var hazards []string
+	if _ghs_hazards.Exists() {
+		if err := json.Unmarshal([]byte(_ghs_hazards.Array()[0].String()), &hazards); err != nil {
+			core.Logger.Error("failed to unmarshal hazards for an ingredient", "name", name, "err", err)
+		}
+	}
+
+	var symptoms []string
+	if _signs_symptoms.Exists() {
+		if err := json.Unmarshal([]byte(_signs_symptoms.Array()[0].String()), &symptoms); err != nil {
+			core.Logger.Error("failed to unmarshal symptoms for an ingredient", "name", name, "err", err)
+		}
+	}
+
+	var effects []string
+	if _adverse_effects.Exists() {
+		if err := json.Unmarshal([]byte(_adverse_effects.Array()[0].String()), &effects); err != nil {
+			core.Logger.Error("failed to unmarshal effects for an ingredient", "name", name, "err", err)
+		}
+	}
+
+	var regulations []string
+	if _california_cosmetics.Exists() {
+		if err := json.Unmarshal([]byte(_california_cosmetics.Array()[0].String()), &regulations); err != nil {
+			core.Logger.Error("failed to unmarshal regulations for an ingredient", "name", name, "err", err)
+		}
+	}
+
+	ingredient := &types.Ingredient{
+		Model: types.Model{
+			Id:      uuid.New(),
+			Created: time.Now(),
+			Updated: time.Now(),
+		},
+
+		PrimaryName: name,
+		Failed:      false,
+	}
+
+	for _, name := range names {
+		ingredient.Names = append(ingredient.Names, types.Name{
+			Model: types.Model{
+				Id:      uuid.New(),
+				Created: time.Now(),
+				Updated: time.Now(),
+			},
+			Text: name,
+		})
+	}
+
+	for _, hazard := range hazards {
+		label := types.Label{
+			Model: types.Model{
+				Id:      uuid.New(),
+				Created: time.Now(),
+				Updated: time.Now(),
+			},
+			Type:    "hazard",
+			Payload: hazard,
+			Origin:  new(string),
+		}
+		*label.Origin = fmt.Sprintf("https://pubchem.ncbi.nlm.nih.gov/compound/%d", pubchem_id)
+		ingredient.Labels = append(ingredient.Labels, label)
+	}
+
+	for _, effect := range effects {
+		label := types.Label{
+			Model: types.Model{
+				Id:      uuid.New(),
+				Created: time.Now(),
+				Updated: time.Now(),
+			},
+			Type:    "effect",
+			Payload: effect,
+			Origin:  new(string),
+		}
+		*label.Origin = fmt.Sprintf("https://pubchem.ncbi.nlm.nih.gov/compound/%d", pubchem_id)
+		ingredient.Labels = append(ingredient.Labels, label)
+	}
+
+	for _, symptom := range symptoms {
+		label := types.Label{
+			Model: types.Model{
+				Id:      uuid.New(),
+				Created: time.Now(),
+				Updated: time.Now(),
+			},
+			Type:    "symptom",
+			Payload: symptom,
+			Origin:  new(string),
+		}
+		*label.Origin = fmt.Sprintf("https://pubchem.ncbi.nlm.nih.gov/compound/%d", pubchem_id)
+		ingredient.Labels = append(ingredient.Labels, label)
+	}
+
+	for _, regulation := range regulations {
+		label := types.Label{
+			Model: types.Model{
+				Id:      uuid.New(),
+				Created: time.Now(),
+				Updated: time.Now(),
+			},
+			Type:    "regulation",
+			Payload: regulation,
+			Origin:  new(string),
+		}
+
+		*label.Origin = fmt.Sprintf("https://pubchem.ncbi.nlm.nih.gov/compound/%d", pubchem_id)
+		ingredient.Labels = append(ingredient.Labels, label)
+	}
+
+	return ingredient, nil
+}
