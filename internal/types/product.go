79f22b739c1c88a18a6010b8273f5a7fed0f2267
Diego Alba <d.alba.work@gmail.com>
2026-05-06 00:33:10 -0400

    feat: added product ownership, private products

--- internal/types/product.go
+++ internal/types/product.go
@@ -1,14 +1,18 @@
 package types
 
+import "github.com/google/uuid"
+
 // A product can be thought of as a list of ingredients with relevant metrics that
 // accompany it, all stemming from the list of ingredients.
 type Product struct {
 	// this is a basic set of database model columns like ID, Created, Updated, and
 	// Deleted.
 	Model
+	UserId *uuid.UUID `json:"user_id" gorm:"type:uuid;"`
 
 	// The following is a name for the product, its origin (e.g. country of origin), and a list of ingredients
 	// (the list of ingredients is a many-to-many relationship, and it is managed via GORM).
+	IsPublic    bool         `json:"is_public" gorm:"default:false"`
 	Name        string       `json:"name" gorm:"type:varchar(255);not null;"`
 	Origin      *string      `json:"origin" gorm:"type:varchar(255);default:null"`
 	Ingredients []Ingredient `json:"ingredients" gorm:"many2many:product_ingredients;"`
